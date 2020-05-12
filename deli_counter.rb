af8cc6c5a448b7d222fb76b343385dc8e1f53d05
Michael Bade <mbade314@gmail.com>
2020-05-12 14:24:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
+  if katz_deli.length >= 1
+    newarray = []
+    counter = 1
+    katz_deli.each do |name|
+      newarray.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{newarray.join(" ")}"
   else
-    puts "The line is currently: #{katz_deli.index()}. #{katz_deli}"
+    puts "The line is currently empty."
   end
 end
