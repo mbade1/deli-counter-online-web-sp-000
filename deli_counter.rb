f03190c2f573cdea0c88a1ee5c7b03f73bc396a5
Michael Bade <mbade314@gmail.com>
2020-05-12 14:27:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length >= 1 
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."  
+end
+  
