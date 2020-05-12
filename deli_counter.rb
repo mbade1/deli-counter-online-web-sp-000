4e2eaafc662e49df368bbcd03a0a6dc2861669fa
Michael Bade <mbade314@gmail.com>
2020-05-12 14:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,6 @@ def take_a_number(katz_deli, name)
   elsif katz_deli.length >= 1
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-    
+
   end
 end
-  
